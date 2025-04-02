26b55a59daec793a358be242ac2516eae3a23c8b
Kyle Measner <kyle@Kyles-MacBook-Pro.local>
2025-04-01 21:06:26 -0700

    Modified formatting a bit and found a way to remove the "[Enter]" text from persisting on the terminal after the program exits.

--- main.go
+++ main.go
@@ -32,29 +32,41 @@ func main() {
 		return
 	}
 
-	fmt.Printf("There are %d questions. ", len(questions))
+	numberOfQuestions := GetNumberToAsk(len(questions))
+	AskQuestions(questions, numberOfQuestions)
+	termenv.DefaultOutput().ClearScreen()
+}
+
+func GetNumberToAsk(numQuestions int) int {
+	output := termenv.DefaultOutput()
+	output.ClearScreen()
+
+	fmt.Println()
+	fmt.Printf("There are %d questions. ", numQuestions)
 
 	number := 0
 	for number < 1 || number > 100 {
 		fmt.Println("How many questions should I ask you?")
-
+		fmt.Print("> ")
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Scan()
-		err = scanner.Err()
+		err := scanner.Err()
 		if err == nil {
 			_, err = fmt.Sscanf(scanner.Text(), "%d", &number)
 		}
+
 		if err != nil || number < 1 || number > 100 {
-			fmt.Printf("\nPlease input a number from 1 to %d. ", len(questions))
+			output.ClearLines(2)
+			output.DeleteLines(2)
+			fmt.Printf("Please input a number from 1 to %d. ", numQuestions)
 		}
 	}
 
-	AskQuestions(questions, number)
+	return number
 }
 
 func AskQuestions(questions []Question, numberToAsk int) {
 	output := termenv.DefaultOutput()
-	enterString := output.String("\n[Enter]").Faint()
 
 	remaining := make([]int, len(questions))
 	for i := range remaining {
@@ -72,14 +84,10 @@ func AskQuestions(questions []Question, numberToAsk int) {
 		questionString := output.String("Question:", question.Question).Bold().Italic()
 		fmt.Println(questionString)
 
-		fmt.Println()
-		fmt.Print(enterString)
-		fmt.Scanln()
-		output.ClearLines(1)
+		WaitForEnter()
 
 		if len(question.Answers) == 1 {
-			answerString := output.String("Answer:", question.Answers[0]).Bold()
-			fmt.Println(answerString)
+			fmt.Println(output.String("Answer:").Bold(), question.Answers[0])
 		} else {
 			fmt.Println(output.String("Possible Answers:").Bold())
 			for _, answer := range question.Answers {
@@ -87,7 +95,16 @@ func AskQuestions(questions []Question, numberToAsk int) {
 			}
 		}
 
-		fmt.Print(enterString)
-		fmt.Scanln()
+		WaitForEnter()
+		fmt.Println()
 	}
+}
+
+func WaitForEnter() {
+	output := termenv.DefaultOutput()
+	fmt.Println()
+	fmt.Print(output.String("\n[Enter]").Faint())
+	fmt.Scanln()
+	output.ClearLines(1)
+	output.DeleteLines(1)
 }
